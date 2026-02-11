f0db8efe8aa5481d4ddf2101194926aab9849a88
unknown <siragenius1@gmail.com>
2026-02-11 23:27:51 +0530

    quicksort

---
+++ Arrays/sortallthenum.java
@@ -0,0 +1,39 @@
+public class sortallthenum {
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+
+    public static void printArray(int[] array) {
+        System.out.println(java.util.Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+        System.out.println("Original array:");
+        printArray(arr);
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
