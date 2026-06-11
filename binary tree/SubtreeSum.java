4212ef1e7fdee40d4813163f5a20000869cb8afb
Your Name <siragenius1@gmail.com>
2026-06-11 11:50:38 +0530

    SubTreeSum

---
+++ binary tree/SubtreeSum.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public  TreeNode(int val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+class SubtreeSum {
+    public static int subtreesum(TreeNode root ,ArrayList<Integer> res){
+        if(root==null){
+            return 0;
+        }
+        int l = subtreesum(root.left ,res);
+        int r = subtreesum(root.right ,res);
+        int sum=root.val + l + r;
+        //res.add(sum);
+       System.out.println(res.add(sum));
+        //System.out.println("Current list: " + res);
+        return  sum;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root =new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+
+        //height sol = new height();
+        ArrayList<Integer> res=new ArrayList<>();
+
+        // System.out.println(Height(root ,res));
+        int totalSum = subtreesum(root, res);
+
+        System.out.println("Total Tree Sum = " + totalSum);
+        System.out.println("Subtree Sums = " + res);
+    }
+}
